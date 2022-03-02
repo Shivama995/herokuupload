df5cff59f9a3cc180eebde2ca88eb86db0351f9e
Shivama995 <shivama995@gmail.com>
2022-03-02 16:40:52 +0530

    in

---
+++ src/main/java/com/upload/HerokuUploadApplication.java
@@ -0,0 +1,13 @@
+package com.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuUploadApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/upload/controller/controller.java
@@ -0,0 +1,15 @@
+package com.upload.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@GetMapping("/")
+	public String get()
+	{
+		return "HII";
+	}
+	
+}
